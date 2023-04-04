f1cf18529ff440e09dc4e1904eb786eecbe5cfd6
Maria Carmo <94873518+malubcarmo@users.noreply.github.com>
2023-04-04 20:11:54 -0300

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/produto', [ProdutoController::class, 'index'])
+->name('produto.index');
